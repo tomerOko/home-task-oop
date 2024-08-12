aef08c5bbe679cd7d25a48cc6f00411cacef1df3
tomerOko <tomeroko2@gmail.com>
2024-08-12 11:42:58 +0300

    first commit

---
+++ src/task/README.md
@@ -0,0 +1,10 @@
+# Refactor `commands-legacy.js` with Object-Oriented approach.
+
+## Workflow guidelines
+* Use TS.
+* Write in this or separate repository.
+
+## Coding guidelines
+* Use classes.
+* Use inheritance.
+* Use factory design pattern.
---
+++ src/task/commands-legacy.ts
@@ -0,0 +1,52 @@
+const util = require('util');
+
+let commandsHashMap = null;
+
+const commonCommandOutput = function () {
+  console.log('Common command is called');
+};
+
+const commands = {
+  help: {
+    name: 'help',
+    aliases: ['sos', 'issue'],
+    init: function () {
+      commonCommandOutput();
+      console.log('Command "help" is called');
+    },
+  },
+  example: {
+    name: 'example',
+    aliases: ['demo', 'test'],
+    init: function () {
+      commonCommandOutput();
+      console.log('Command "example" is called');
+    },
+  },
+
+  getCommand: function (name) {
+    var com =
+      commandsHashMap[util.isString(name) ? name.toLowerCase().trim() : name];
+    if (com) {
+      return com;
+    }
+    return false;
+  },
+
+  buildCommands: function () {
+    commandsHashMap = {};
+    var keys = Object.keys(this);
+    var len = keys.length;
+    for (var i = 0; i < len; i++) {
+      if (this[keys[i]].aliases) {
+        commandsHashMap[this[keys[i]].name] = this[keys[i]];
+        for (var j = 0; j < this[keys[i]].aliases.length; j++) {
+          var newKey = this[keys[i]].aliases[j].toLowerCase();
+          commandsHashMap[newKey] = this[keys[i]];
+        }
+      }
+    }
+  },
+};
+
+module.exports = commands;
